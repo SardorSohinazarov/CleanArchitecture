6e0410522fa04c60bdbfbad7909d43a87dcf6c95
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-04 15:18:34 +0500

    Bitta notification implemented

---
+++ src/Application/Notifications/ProductCreateNotification.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using MediatR;
+using System.Text.Json;
+
+namespace Application.Notifications
+{
+    public class ProductCreateNotification : INotification
+    {
+        public Product Product { get; set; }
+    }
+
+    public class ProductCreateNotificationHandler : INotificationHandler<ProductCreateNotification>
+    {
+        public Task Handle(ProductCreateNotification notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Shu maxsulot qo'shildida qara:" + JsonSerializer.Serialize(notification.Product));
+
+            return Task.CompletedTask;
+        }
+    }
+}
